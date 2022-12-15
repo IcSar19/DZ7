347faa0b5b81dbfbc37dab1c9b699606640b17f2
Daniel <daniel.bozhko1903@mail.ru>
2022-12-15 22:22:05 +0700

    Result

---
+++ Example1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите количество строк  ");
+int rows = Convert.ToInt32(ReadLine());
+WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(ReadLine());
+double[,] numbers = new double[rows, columns];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write(array[i, j] + " ");
+        }
+        Write("]");
+        WriteLine("");
+    }
+}
---
+++ Example2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int rows = ReadInt("Введите индекс строки: ");
+int colums = ReadInt("Введите индекс столбца: ");
+int[,] numbers = new int[6, 8];
+FillArray(numbers);
+PrintArray(numbers);
+
+if (rows < numbers.GetLength(0) && colums < numbers.GetLength(1))
+{
+    WriteLine(numbers[rows, colums]);
+}
+else
+{
+    WriteLine($"{rows}{colums} -> такого числа в массиве нет");
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write(array[i, j] + " ");
+        }
+        WriteLine();
+    }
+    WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Write(message);
+    return Convert.ToInt32(ReadLine());
+}
